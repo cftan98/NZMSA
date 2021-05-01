af32e3dacf434ee04c723e75d5e8c14ef0f265bc
cftan98 <cftan98@users.github.com>
2021-05-01 22:09:04 +1200

    Change the counter size and add notification when unlock

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import 'antd/dist/antd.css';
-import { Row, Col } from 'antd';
+import { Row, Col, Image, notification } from 'antd';
+import Lock from './verouillageautomatique10.png';
+import UnLock from './verouillageautomatique5.png';
 import Counter from './components/Counter';
 
 const PASSCODE = 857;
@@ -9,17 +11,24 @@ function App() {
   const [value1, setValue1] = useState(0);
   const [value2, setValue2] = useState(0);
   const [value3, setValue3] = useState(0);
+  const [pass, setPass] = useState(false);
 
   useEffect(() => {
     const result = value1 * 100 + value2 * 10 + value3
-    if(result === PASSCODE){
-      alert("!")
+    if (result === PASSCODE) {
+      setPass(true);
+      notification['success']({
+        message: 'Pass!',
+        description: 'You have unlocked the lock!'
+      });
+    } else {
+      setPass(false);
     }
   }, [value1, value2, value3])
 
   return (
     <div>
-      <Row justify='center' style={{marginTop: "20%"}}>
+      <Row justify='center' style={{ marginTop: "20%" }}>
         <Col span={2}>
           <Counter num={value1} handleChange={(e) => setValue1(e)} />
         </Col>
@@ -28,6 +37,15 @@ function App() {
         </Col>
         <Col span={2}>
           <Counter num={value3} handleChange={(e) => setValue3(e)} />
+        </Col>
+      </Row>
+
+      <br />
+      <br />
+
+      <Row justify='center'>
+        <Col>
+          {!pass ? <Image src={Lock} alt={"Locked!"} width="50px" /> : <Image src={UnLock} alt={"Unlocked!"} width="50px" />}
         </Col>
       </Row>
     </div>
--- web-app/src/components/Counter.jsx
+++ web-app/src/components/Counter.jsx
@@ -4,6 +4,6 @@ import { InputNumber } from 'antd';
 export default function Counter(props) {
 
     return (
-        <InputNumber min={0} max={9} value={props.num} onChange={(e) => props.handleChange(e)} />
+        <InputNumber size='large' min={0} max={9} value={props.num} onChange={(e) => props.handleChange(e)} />
     )
 }
