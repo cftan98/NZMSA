a7c84e47b9905d3ca009d1583ba1e4e16e4d9055
cftan98 <cftan98@users.github.com>
2021-05-01 21:14:13 +1200

    Added three counter with allowed range

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import 'antd/dist/antd.css';
+import { Row, Col, InputNumber } from 'antd';
+import Counter from './components/Counter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Hello World</h2>
-      </header>
+    <div>
+      <Row justify='center' style={{marginTop: "20%"}}>
+        <Col span={8}>
+          <InputNumber min={0} max={9} />
+        </Col>
+        <Col span={8}>
+          <InputNumber min={0} max={9} />
+        </Col>
+        <Col span={8}>
+          <InputNumber min={0} max={9} />
+        </Col>
+      </Row>
+
     </div>
   );
 }
